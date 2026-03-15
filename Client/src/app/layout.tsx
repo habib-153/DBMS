68216e197bf2a8665992941c089b2204f66e1e98
habib-153 <h.r.sihab155@gmail.com>
2026-03-15 11:30:23 +0600

    feat: layout component updated

--- Client/src/app/layout.tsx
+++ Client/src/app/layout.tsx
@@ -33,6 +33,11 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8875395790160173"
+          crossorigin="anonymous"
+        ></script>
         <link href="//fonts.googleapis.com" rel="dns-prefetch" />
         <link
           crossOrigin=""
@@ -49,7 +54,7 @@ export default function RootLayout({
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable,
-          fontExo.variable
+          fontExo.variable,
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
